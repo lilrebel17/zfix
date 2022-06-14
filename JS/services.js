eb5953c3a4ff8a5e1e3b3b7e4f8e0a06bef67e97
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-06-14 14:03:18 -0400

    First Draft complete
    Completed the first draft of the redesign.

--- JS/services.js
+++ JS/services.js
@@ -1,61 +1,51 @@
-moreInfoBtnCntnr = document.getElementById('why-btn-cntnr')
-moreInfoCloseButton = document.getElementById('why-slide-close-btn')
-SlideShowContainer = document.querySelector('#why-slide-cntnr')
-slideImageElement = document.querySelector('.why-slide-img')
-slideImageDescription = document.querySelector('.why-slide-desc')
-whyHeader = document.querySelector('#why-hdr')
+const moreInfoBtn = document.querySelector('#why-btn-cntnr')
+const whyHeader = document.querySelector('#why-hdr')
+const whyCloseBtn = document.querySelector('#why-slide-close-btn')
+const whySlide = document.querySelector('#why-slide-cntnr')
+
+//Always listening for the more info button to be clicked'
+moreInfoBtn.addEventListener('click', () => {
+    //starts off at 150% right, this is so it goes to the middle of the page
+    whySlide.style.right = '0'
+    whySlide.style.transform = 'scale(1)'
+
+    //Returns the slide to relative so it fits within the document again
+    //transitionrun is the 1st step in a transition, so this fires immediatley
+    //before any delays in a transition
+    whySlide.addEventListener('transitionrun', function setRelative() {
+        whySlide.style.position = 'relative'
+
+        //Causes issues if not removed, doubles the animation on first click
+        whySlide.removeEventListener('transitionrun', setRelative);
+    })
+
+    //transitionstart is after it starts, a small delay then this fires
+    whySlide.addEventListener('transitionstart', function removeheader() {
+        whyHeader.style.transform = 'scale(0)'
+        whyHeader.style.position = 'absolute'
+
+        whySlide.removeEventListener('transitionstart', removeheader)
+    })
 
 
-SlideShowArray = [
-    '.\Resources\computer-repair.jpg'
-]
-
-moreInfoBtnCntnr.addEventListener('click', (e) => {
-    if(e.target == moreInfoBtnCntnr || document.getElementById('why-btn')) {
-        SlideShowContainer.classList.remove('invisible')
-        SlideShowContainer.classList.add('visible')
-
-        moreInfoBtnCntnr.classList.add('invisible')
-        moreInfoBtnCntnr.classList.remove('visible')
-
-        whyHeader.classList.add('invisible')
-        whyHeader.classList.remove('visible')
-
-        SlideShowContainer.style.right = '0%';
-        whyHeader.style.left = '150%'
-        moreInfoBtnCntnr.style.left = '150%'
-    }
 })
 
-moreInfoCloseButton.addEventListener('click', () =>{
-   SlideShowContainer.classList.remove('visible')
-   SlideShowContainer.classList.add('invisible')
 
-   moreInfoBtnCntnr.classList.add('visible')
-   moreInfoBtnCntnr.classList.remove('invisible')
+//Always listening for the close button to be clicked
+whyCloseBtn.addEventListener('click', () => {
 
-   whyHeader.classList.add('visible')
-   whyHeader.classList.remove('invisible')
+    whySlide.style.right = '150%'
+    whySlide.style.transform = 'scale(0)'
 
-   SlideShowContainer.style.right = "102%"
-   whyHeader.style.left = '0%'
-   moreInfoBtnCntnr.style.left = '0%'
-})
+    //This sets the slide in element to absolute when its finished
+    whySlide.addEventListener('transitionend', function setAbsolute() {
+        //Once the transition ends, it sets the element to absolute, and rescales it to 1
+        whySlide.style.position = 'absolute'
+        whyHeader.style.position = 'relative'
+        whyHeader.style.transform = 'scale(1)'
 
-whyHeader.addEventListener('transitionstart', () => {
-    if(whyHeader.style.left == '0%') {
-        whyHeader.style.height = 'initial'
-    }
-    if(whyHeader.style.left == '150%') {
-        whyHeader.style.height = '0px'
-    }
-})
+        //Have to remove this event listener or it will set this element to absolute on any transition
+        whySlide.removeEventListener('transitionend', setAbsolute)
+    })
 
-moreInfoBtnCntnr.addEventListener('transitionstart', () => {
-    if(moreInfoBtnCntnr.style.left == '150%') {
-        moreInfoBtnCntnr.style.height= '0px'
-    }
-    if(moreInfoBtnCntnr.style.left == '0%') {
-        moreInfoBtnCntnr.style.height = 'initial'
-    }
 })
--- README.md
+++ README.md
@@ -2,45 +2,32 @@
 
  Z-Fix, formerly ZF Computer Services is an electronics repair company. Focused mainly on computer and phone repairs. Located in Easley, South Carolina.
 
+## Tech Stack
+
+- No frameworks, all vanilla
+  - HTMl
+  - CSS
+  - JS
+
 ## Requirements
 
-   1. Focus on SEO
-   1. Focus on accessibility
-   1. Focus on browser compatibility
-   1. Easy to navigate
-   1. Smooth transitions & animation
-   1. Simplistic design
-   1. Mobile & Desktop versions
+ 1. Animations
+ 2. Fully Respsonsive
+ 3. Fully accessible
+ 4. Full Browser compatibility
+ 5. Minamilistic design
+ 6. Custom logo
+ 7. Designed for readability & easy updates
 
-## The Process
 
-  1. HTML Skeletons
-      * [X] landing.html
-      * [X] landing.html
-      * [X] services.html
-      * [X] home.html
-      * [X] contact.html
-      * [X] mission.html
-  2. HTML Accessability
-      * [ ] landing.html
-      * [ ] services.html
-      * [ ] home.html
-      * [ ] contact.html
-      * [ ] mission.html
-  3. CSS
-      1. [X] Mobile Version
-      * [X] landing.html
-      * [X] services.html
-      * [X] home.html
-      * [X] contact.html
-      * [X] mission.html
-      1. [ ] Desktop Version
-      * [ ] landing.html
-      * [ ] services.html
-      * [ ] home.html
-      * [ ] contact.html
-      * [ ] mission.html
-  4. Cleanup
-      * [ ] Code Cleanup
-      * [ ] Browser Compatability Testing
-      * [ ] Front End Checklist Completion
+## To-Do
+
+- [X] Create a custom logo
+- [X] Finish first draft
+- [ ] Confirm final draft
+- [ ] Make fully respsonsive
+- [ ] Complete accessability requirement
+- [ ] Test on all browsers
+- [ ] Insure readability
+- [ ] Lint all documents
+- [ ] Get rid of un-used code
