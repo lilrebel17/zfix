bb26eccc2991eeed6fb5cc681fa3e4c906e07c25
Zack Farmer <zackfarmer1710@gmail.com>
2024-07-13 22:31:22 -0400

    About Us Section
    - Added About Us section
    - Added AboutUs.tsx
    	- Includes header and content talking about the business
    - Imported & Initalized AboutUS in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import '../global.css'
+
+const AboutUs = () => {
+    return (
+        <div className="text-center mt-1">
+            <h2><b>About Us</b></h2>
+            <p> 
+            Founded by an experienced IT professional specializing in hardware repair and systems administration, we operate from a home-based workshop, 
+            ensuring personalized attention and swift solutions for your technology needs. 
+            Our commitment to quality is reflected in our use of premium parts and effective repair methods, ensuring your devices perform optimally. 
+            As a local business, we prioritize building lasting relationships through transparent service and competitive pricing. 
+            Trust <span className="text-green"><b>Z-FIX</b></span> for reliable, professional tech repairs.
+            </p>
+        </div>
+    )
+}
+
+export default AboutUs
