b358ffa8670c2143446d91e24f22b7d14133dd01
Zack Farmer <zackfarmer1710@gmail.com>
2024-07-13 21:56:22 -0400

    Logo and Banner updates
    - Added height in style tag so it would size correctly.
    - Removed plain text phone number.
    - Changed header phone number to correct phone number
    - Changed to "Text Anytime" from "Call Now"
    - Added sms: to href. So it defaults to your texting app on Mobile.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,12 @@ import '../global.css'
 const Header = () => {
     return(
         <div className="container-fluid mb-0 text-center fs-6 mb-1 p-0">
-            <div className="text-center bg-green text-white m-0 p-0">
+            <div className="text-center bg-green text-white m-0 pb-2">
                 <span>Proudly Serving Upstate South Carolina!</span>
                 <br/>
-                <span><b>Call now!</b></span>
-                <a href="tel:8644040591" className="text-white">
-                    <p className="pb-1 mb-1"><b>864.404.0591</b></p>
-                </a>
+                <a href="sms:8643729283" className="link-light"><b>Text Anytime</b></a>
             </div>
-            <img src='./resources/ZFix Logo.png' alt="Z-FIX Logo" className="img-fluid"></img>
+            <img src='./resources/ZFix Logo.png' alt="Z-FIX Logo" className="img-fluid" style={{height: "15rem"}}></img>
         </div>
     )
 }
