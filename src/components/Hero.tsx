13855c77d45b7474b30a1d569d9063e6be45b444
Zack Farmer <zackfarmer1710@gmail.com>
2024-07-13 22:13:20 -0400

    Hero Section Rewrite
    - Added a new tagline.
    - Rewriting the Hero Section to better reflect the businesses mission
    - Added "Call now" button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,17 @@ import '../global.css'
 const Hero = () => {
     return (
         <div className="container-fluid bg-soft-gray text-center p-3">
-            <h4 className="h4"><b>Phone, laptop or desktop issues?</b></h4>
+            <h4 className="h4"><b>Expert Repairs, Exceptional Service: Your Trusted Technology Partner in South Carolina</b></h4>
             <p className="fs-6">
-                <span className="text-green"><b>Z-FIX</b></span> specializes in computer, tablet and electronics repair!
-                We tailor every repair to <b>YOUR NEEDS</b>. We will work with you on getting the best solution for your price point!
+                Welcome to <span className="text-green"><b>Z-FIX</b></span>, your premier destination for expert computer, phone, 
+                and desktop repair services in upstate South Carolina. Founded and operated by a dedicated IT professional with extensive experience in 
+                system administration and hardware repair. We specialize in fast, reliable solutions from our home-based workshop. 
+                Whether you're dealing with hardware malfunctions, software issues, or need device optimization advice, 
+                our knowledgeable team ensures your technology works seamlessly. Contact us today for expert tech repairs.
             </p>
+            <a href='tel:8643729283'>
+                <button type='button' className='btn btn-lg btn-success'>Call Now</button>
+            </a>
         </div>
     )
 }
